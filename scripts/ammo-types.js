f923525d00c3b2cceed8b40e07ffd80071444b99
Jeremy Allan <jallan8210@gmail.com>
2026-01-16 10:30:51 -0500

    overhaul with settings added

--- scripts/ammo-types.js
+++ scripts/ammo-types.js
@@ -1,77 +1,68 @@
-
-
-
-
 export default ammoTypes = {
-  'gb-ammo-light': {
+  // v1.0.0 - Original ammo types
+  'gb-ammo-light-rounds': {
     parent: null,
-    label: "GB's Light Ammo",
+    label: "GB's Light Rounds",
     magazine: false,
     stackGroup: null,
     weapon: null
   },
-  'gb-ammo-medium': {
+  'gb-ammo-medium-rounds': {
     parent: null,
-    label: "GB's Medium Ammo",
+    label: "GB's Medium Rounds",
     magazine: false,
     stackGroup: null,
     weapon: null
   },
-  'gb-ammo-heavy': {
+  'gb-ammo-heavy-rounds': {
     parent: null,
-    label: "GB's Heavy Ammo",
+    label: "GB's Heavy Rounds",
     magazine: false,
     stackGroup: null,
     weapon: null
   },
-  'gb-ammo-shell': {
+  'gb-ammo-shells': {
     parent: null,
-    label: "GB's Shotgun Shells",
+    label: "GB's Shells",
     magazine: false,
     stackGroup: null,
     weapon: null
   },
-  'gb-ammo-micro-rockets': {
+  'gb-ammo-micro-missiles': {
     parent: null,
-    label: "GB's Micro Rockets",
+    label: "GB's Micro Missiles",
     magazine: false,
     stackGroup: null,
     weapon: null
   },
-
-  // 'gb-ammo-light': {
-  //   parent: null,
-  //   label: "GB's Light Ammo",
-  //   magazine: true,
-  //   stackGroup: null,
-  //   weapon: null
-  // },
-  // 'gb-ammo-medium': {
-  //   parent: null,
-  //   label: "GB's Medium Ammo",
-  //   magazine: true,
-  //   stackGroup: null,
-  //   weapon: null
-  // },
-  // 'gb-ammo-heavy': {
-  //   parent: null,
-  //   label: "GB's Heavy Ammo",
-  //   magazine: true,
-  //   stackGroup: null,
-  //   weapon: null
-  // },
-  // 'gb-ammo-shell': {
-  //   parent: null,
-  //   label: "GB's Shotgun Shells",
-  //   magazine: true,
-  //   stackGroup: null,
-  //   weapon: null
-  // },
-  // 'gb-ammo-micro-rockets': {
-  //   parent: null,
-  //   label: "GB's Micro Rockets",
-  //   magazine: true,
-  //   stackGroup: null,
-  //   weapon: null
-  // },
-}
+  
+  // v1.1.0 - Added specialized ammo types
+  'gb-ammo-darts': {
+    parent: null,
+    label: "GB's Darts",
+    magazine: false,
+    stackGroup: null,
+    weapon: null
+  },
+  'gb-ammo-bolts': {
+    parent: null,
+    label: "GB's Bolts",
+    magazine: false,
+    stackGroup: null,
+    weapon: null
+  },
+  'gb-ammo-rail-slugs': {
+    parent: null,
+    label: "GB's Rail Slugs",
+    magazine: false,
+    stackGroup: null,
+    weapon: null
+  },
+  'gb-ammo-cards': {
+    parent: null,
+    label: "GB's Cards",
+    magazine: false,
+    stackGroup: null,
+    weapon: null
+  }
+};
